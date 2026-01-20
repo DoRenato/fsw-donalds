2813e49f39387898338737868b7ff7b08ad553a1
DoRenato <64191958+DoRenato@users.noreply.github.com>
2026-01-20 05:51:37 -0300

    feat: adiciona página dinâmica de produto com cabeçalho dedicado
    Introduz uma página dinâmica de produto em /[slug]/menu/[productId] que busca os dados do produto e do restaurante, valida o método de consumo e renderiza as informações corretamente. Adiciona o novo componente ProductHeader, responsável por exibir a imagem do produto com botões de navegação e ações informativas.

---
+++ app/[slug]/menu/[productId]/page.tsx
@@ -0,0 +1,29 @@
+import ProductHeader from "@/components/product/image-header-with-buttons";
+import { getProductById, getRestaurantBySlug } from "@/data/get-restaurant-data";
+import { isConsumptionMethodValid } from "@/utils/is-comsumption-method-valid";
+import { notFound } from "next/navigation";
+
+interface ProductPageProps {
+  params: Promise<{ slug: string; productId: string }>;
+  searchParams: Promise<{
+    consumptionMethod: string;
+  }>;
+}
+
+export default async function ProductPage({
+  params,
+  searchParams,
+}: ProductPageProps) {
+  const { slug, productId } =  await params;
+  const restaurant = await getRestaurantBySlug(slug);
+  const product = await getProductById(productId);
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod) || !product) {
+    return notFound();
+  }
+  return (
+    <main className="container mx-auto">
+      <ProductHeader product={product}/>
+    </main>
+  );
+}
---
+++ components/product/image-header-with-buttons.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Product, Restaurant } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface ProductHeaderProps {
+  product: Pick<Product, "name" | "imageUrl">; // Só chama essas propriedades ao inves de todas
+}
+
+export default function ProductHeader({ product }: ProductHeaderProps) {
+  const router = useRouter();
+  return (
+    <div className="relative inline-block"> {/* inline block serve para quando tem botoes dentro de uma imagem */}
+      <div className="bg-gray-200 p-5">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={415}
+          height={415}
+          className="aspect-square object-cover"
+        />
+      </div>
+      <Button
+        variant={"secondary"}
+        size={"icon"}
+        className="absolute left-4 top-4 z-50 rounded-full bg-white"
+        onClick={() => router.back()}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      <Button
+        variant={"secondary"}
+        size={"icon"}
+        className="absolute right-4 top-4 z-50 rounded-full bg-white"
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
+  );
+}
